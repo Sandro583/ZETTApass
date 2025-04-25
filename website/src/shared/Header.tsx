a856e9aed17a2e1ed38cb1213f0add33ac73049a
Sandro583 <sandro.greter@zettaplan.ch>
2025-04-25 15:05:41 +0200

    Anpassung Header
    Anpassung des Namen Yopass zu ZETTApass und positionierung des Text im Header

--- website/src/shared/Header.tsx
+++ website/src/shared/Header.tsx
@@ -9,31 +9,28 @@ export const Header = () => {
   const base = process.env.PUBLIC_URL || '';
   const home = base + '/#/';
   const upload = base + '/#/upload';
+
   return (
     <AppBar position="static" color="transparent" sx={{ marginBottom: 4 }}>
       <Toolbar>
         <Link href={home} color="inherit" underline="none">
-          <Typography variant="h6" component="div">
-            Yopass
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
             <Box
+              component="img"
               sx={{
-                verticalAlign: 'middle',
-                paddingLeft: '5px',
                 width: '40px',
                 height: '40px',
+                marginRight: '8px',
               }}
-              component="img"
-              height="40"
               alt=""
               src="yopass.svg"
             />
-          </Typography>
+            <Typography variant="h6" component="div">
+              ZETTApass
+            </Typography>
+          </Box>
         </Link>
-        <Box
-          sx={{
-            marginLeft: 'auto',
-          }}
-        >
+        <Box sx={{ marginLeft: 'auto' }}>
           <Button
             component={Link}
             href={isOnUploadPage ? home : upload}
